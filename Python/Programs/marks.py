33a09a8b3bb9776afeae59ac9ef4db30bf945dd0
ghost-man01 <ghost_man01@wearehackerone.com>
2022-09-01 11:47:11 +0530

    Some changes in marks program

--- Python/Programs/marks.py
+++ Python/Programs/marks.py
@@ -1,13 +1,14 @@
 nameOfStudent = str(input("Enter your Name: "))
 marksOfStudent = int(input("Enter your Marks: "))
+stringofMarks = str(marksOfStudent)
 
 if (marksOfStudent > 85 and marksOfStudent <= 100):
-    print(nameOfStudent,"A")
+    print(nameOfStudent,stringofMarks,"A")
 elif (marksOfStudent > 60 and marksOfStudent <= 85):
-    print(nameOfStudent,"B+")
+    print(nameOfStudent,stringofMarks,"B+")
 elif (marksOfStudent > 40 and marksOfStudent <= 60):
-    print(nameOfStudent,"B")
+    print(nameOfStudent,stringofMarks,"B")
 elif (marksOfStudent > 30 and marksOfStudent <= 40):
-    print(nameOfStudent,"C")
+    print(nameOfStudent,stringofMarks,"C")
 else:
-    print("Fail")
+    print(nameOfStudent,stringofMarks,"Fail")
